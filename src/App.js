ef4e0d780d1c028ec52bbe3c4dbbf4e21dedbdcc
escribapetrus <schreiber.mmb@gmail.com>
2019-12-17 14:36:03 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
+import Header from "./components/Header"
 import './App.css';
 
 const Hats = () => (
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/hats" component={Hats}/>
