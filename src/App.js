c6e2d524d5c42f17d2db7ab8e093ee5d75db7cbd
escribapetrus <schreiber.mmb@gmail.com>
2019-12-27 15:39:13 -0300

    redirect signin, separate action types to file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import {connect} from 'react-redux'
 
 import Home from "./pages/Home"
@@ -49,7 +49,11 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/shop" component={Shop}/>
-            <Route path="/signin" component={SignIn}/>
+            <Route exact path="/signin" render={
+              () =>
+                this.props.currentUser ? (<Redirect to="/"/>)
+                : (<SignIn/>)
+            }/>
           </Switch>
         </Router>    
       </div>
@@ -57,8 +61,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
