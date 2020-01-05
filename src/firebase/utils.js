ade2b01e5bd5f920537c695fc82ce81dce2aef1c
escribapetrus <schreiber.mmb@gmail.com>
2020-01-05 12:42:14 -0300

    add stuff to env variables

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyASOg0aysbyy-BRZYqviD-yv583bUbf-ZE",
-    authDomain: "crwn-db-1ae0f.firebaseapp.com",
-    databaseURL: "https://crwn-db-1ae0f.firebaseio.com",
-    projectId: "crwn-db-1ae0f",
-    storageBucket: "crwn-db-1ae0f.appspot.com",
-    messagingSenderId: "494014378533",
-    appId: "1:494014378533:web:b967dd1632d1c0a409c5cc"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
